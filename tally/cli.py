cf0232e3f676c38295888fc6306f0eb90460a85c
Nathanael Raway <nathanael.raway@gmail.com>
2020-12-21 09:04:48 -0600

    Functional with goals

--- tally/cli.py
+++ tally/cli.py
@@ -7,7 +7,7 @@ import questionary
 
 from tally.storage import Storage
 
-storage = Storage()
+storage = Storage("sqlite:///tally.db")
 
 # TODO: change sleep to something like sleep_until
 # see https://stackoverflow.com/questions/2031111/in-python-how-can-i-put-a-thread-to-sleep-until-a-specific-time
@@ -18,22 +18,36 @@ storage = Storage()
 INTERVAL_MINUTES = 15
 
 INTERVAL_SECONDS = INTERVAL_MINUTES * 60
-INTERVAL_SECONDS = 2
+# INTERVAL_SECONDS = 1
 
-ALLIGN_TO_HOUR = False
+ALLIGN_TO_HOUR = True
 SELECT_DIALOG = True
 
 activities = [
-    "Body Squats",
-    "Pushups",
-    "Pullups",
+    {
+        "label": "Body Squats",
+        "goal": 10,
+    },
+    {
+        "label": "Pushups",
+        "goal": 10,
+    },
+    {
+        "label": "Pullups",
+        "goal": 10,
+    },
 ]
+
+def run_applescript(applescript):
+    return str(subprocess.check_output(f"osascript -e '{applescript}'", shell=True))
 
 def get_user_activity(activities, status="", default=None):
     # messagebox.showinfo("Title", "message")
     # activities_str = '"' + '", "'.join(activities + ["Nothing"]) + '"'
     activities_str = '"' + '", "'.join(activities) + '"'
     # click.echo(activities_str)
+
+    prompt = "What did you do this time?"
 
     if default is None:
         default = activities[0]
@@ -42,7 +56,7 @@ def get_user_activity(activities, status="", default=None):
 
         applescript = f"""
         set choices to {{{activities_str}}}
-        set choice to choose from list choices with prompt "What did you do this time?" default items {{"{default}"}}
+        set choice to choose from list choices with prompt "{status}{prompt}" default items {{"{default}"}}
         choice
         """
         selection = subprocess.check_output(f"osascript -e '{applescript}'", shell=True).decode().strip()
@@ -56,13 +70,14 @@ def get_user_activity(activities, status="", default=None):
 
     else:
         applescript = f"""
-        display dialog "{status}What did you do this time?" ¬
+        display dialog "{status}{prompt}" ¬
         with title "Tally" ¬
         with icon stop ¬
         giving up after {INTERVAL_SECONDS - 60} ¬
         buttons {{{activities_str}}}
         """
-        selection = str(subprocess.check_output(f"osascript -e '{applescript}'", shell=True))
+        # selection = str(subprocess.check_output(f"osascript -e '{applescript}'", shell=True))
+        selection = run_applescript(applescript)
         if "gave up:true" in selection:
             return False
 
@@ -71,7 +86,14 @@ def get_user_activity(activities, status="", default=None):
 
         return selection
 
-
+def tell_finished():
+    message = "You met your goals for the day!"
+    click.echo(message)
+    applescript = f"""
+    set message to "{message}"
+    display dialog message buttons {"Great!"}"
+    """
+    run_applescript(applescript)
 
 def sleep_loop():
     if ALLIGN_TO_HOUR:
@@ -121,14 +143,19 @@ def run():
                 sleep_loop()
 
         status = ""
-        status += "Today, you have done:"
+        # status += "Today, you have done:"
+        default_activity = activities[0]
         for activity in activities:
-
-            status += f"\n{activity}: {storage.get_count(activity)}"
-        status += "\n\n"
+            activity["count"] = storage.get_count(activity["label"])
+            if activity["count"] < default_activity["count"]:
+                default_activity = activity
+            status += f'{activity["label"]}: {activity["count"]}/{activity["goal"]}\n'
+        # status += "\n\n"
 
         click.echo(status)
-        selection = get_user_activity(activities, status=status, default=activities[1])
+        click.echo(f'Do next: {default_activity["label"]}')
+        activities_list = (a["label"] for a in activities)
+        selection = get_user_activity(activities_list, status=status, default=default_activity["label"])
 
         if selection:
             click.echo(selection)
@@ -143,6 +170,16 @@ def run():
         #     choices=activities
         # ).ask()
         # click.echo("Great job!")
+
+        loop = False
+        for activity in activities:
+            activity["count"] = storage.get_count(activity["label"])
+            if activity["count"] < activity["goal"]:
+                loop = True
+        if not loop:
+            tell_finished()
+            break
+
 
         sleep_loop()
         # selection = click.prompt("What did you do this time?", type=str)
